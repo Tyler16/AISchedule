7e22ba60cdc278fef39f870bf7bf07c7c8e5c4d6
TygerTy1 <tylerpautler@gmail.com>
2023-07-07 19:58:53 -0400

    Tried to get oauth to work(it did not)

--- back-end/reschedule/models.py
+++ back-end/reschedule/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Event(models.Model):
+    uid = models.CharField(max_length=64)
     name = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    
+    from_task = models.BooleanField(default=False)
+
+class DailyTask(models.Model):
+    uid = models.CharField(max_length=64)
+    name = models.CharField(max_length=200)
+    date = models.DateField()
+    completed = models.BooleanField(default=False)
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
