051465cf11b1c0ce16d05a327537b044de97c755
TygerTy1 <tylerpautler@gmail.com>
2023-08-07 19:08:57 -0400

    Set up basic calendar system

---
+++ front-end/src/components/Calendar.jsx
@@ -0,0 +1,30 @@
+'use client'
+
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  WeekView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
+
+export default () => (
+  <Paper>
+    <Scheduler
+      data={schedulerData}
+    >
+      <ViewState/>
+      <DayView startDayHour={9} endDayHour={19}/>
+      <WeekView startDayHour={9} endDayHour={19} />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+);
