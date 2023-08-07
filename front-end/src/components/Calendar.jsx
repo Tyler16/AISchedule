79ba7ae239e36051e499b255052db1b989509e36
TygerTy1 <tylerpautler@gmail.com>
2023-08-07 19:47:13 -0400

    Added view switching

--- front-end/src/components/Calendar.jsx
+++ front-end/src/components/Calendar.jsx
@@ -7,23 +7,32 @@ import {
   Scheduler,
   DayView,
   WeekView,
+  MonthView,
+  Toolbar,
+  DateNavigator,
+  TodayButton,
+  ViewSwitcher,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 const currentDate = '2018-11-01';
 const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2023-08-07T09:45', endDate: '2023-08-07T11:00', title: 'Meeting' },
   { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
 ];
 
 export default () => (
   <Paper>
-    <Scheduler
-      data={schedulerData}
-    >
+    <Scheduler data={schedulerData}>
       <ViewState/>
-      <DayView startDayHour={9} endDayHour={19}/>
-      <WeekView startDayHour={9} endDayHour={19} />
+      <DayView startDayHour={0} endDayHour={23}/>
+      <WeekView startDayHour={0} endDayHour={24} />
+      <MonthView />
+
+      <Toolbar />
+      <DateNavigator />
+      <TodayButton />
+      <ViewSwitcher />
       <Appointments />
     </Scheduler>
   </Paper>
