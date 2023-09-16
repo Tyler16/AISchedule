993c26ffb112d58e7f02cecbe32eb7a2729850d0
TygerTy1 <tylerpautler@gmail.com>
2023-09-16 09:04:35 -0400

    Added case for empty TodoList

--- frontend/src/components/todo/TodoList.tsx
+++ frontend/src/components/todo/TodoList.tsx
@@ -5,6 +5,9 @@ import { ToDoItem, TodoListProps } from '../types';
 export default function TodoList(props: TodoListProps) {
   return (
     <div className="w-full border-b-2 h-3/4 overflow-auto">
+      {props.todoItems.length == 0 && 
+        <p>Add a todo item and it will show up here</p>
+      }
       {props.todoItems.map((item: ToDoItem) => <TodoItem item={item} key={item.id} deleteFunction={props.deleteFunction}/>)}
     </div>
   )
