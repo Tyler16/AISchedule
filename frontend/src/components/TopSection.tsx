379d25cf384902c7f2a5c31be01ebcf2620f6bae
TygerTy1 <tylerpautler@gmail.com>
2023-09-18 05:43:50 -0400

    Moved logout button to right corner

--- frontend/src/components/TopSection.tsx
+++ frontend/src/components/TopSection.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/Calendar_icon_2.svg'
 export default function TopSection() {
   return (
     <div className="h-1/12 w-full flex flex-row justify-center items-center border-b-2 border-gray-200">
-      <div className="flex flex-row justify-center items-center">
+      <div className="w-full flex flex-row justify-center items-center">
         <img src={logo} className="w-8"></img>
         <h1 className='text-cyan-400 text-xl'>AutoSchedule</h1>
       </div>
