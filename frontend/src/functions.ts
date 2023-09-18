1e700ef499eb49c24b04ec21e41f959243b4825a
TygerTy1 <tylerpautler@gmail.com>
2023-09-17 22:38:05 -0400

    set up basic heap

---
+++ frontend/src/functions.ts
@@ -0,0 +1,21 @@
+class Heap {
+  private heap: any[];
+  private sortFunction: Function;
+
+  public constructor(heap: any[], sortFunction: Function) {
+    this.heap = heap;
+    this.sortFunction = sortFunction;
+  }
+
+  public heapify() {
+
+  }
+
+  public heappop(): any {
+
+  }
+  
+  public heappush() {
+
+  }
+}
